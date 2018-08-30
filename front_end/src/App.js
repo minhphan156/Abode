ab9ca9b7a488105de2cdccdf958dc917128da49c
Minh Phan <minhphan156@gmail.com>
2018-08-29 20:43:13 -0700

    removed libraries vscode

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import NavBar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div className='App'>
+          <NavBar />
+          <Route exact path='/' component={Landing} />
+          <Footer />
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
