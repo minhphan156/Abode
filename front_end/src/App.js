6a40ceaa3e31e102db4a4e370b4df77ce0cc6cad
Minh Phan <minhphan156@gmail.com>
2018-08-30 11:57:17 -0700

    finished register no redux

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import NavBar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 import "./App.css";
 
 class App extends Component {
@@ -12,6 +16,10 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
           <Footer />
         </div>
       </Router>
--- front_end/src/components/layout/Navbar.js
+++ front_end/src/components/layout/Navbar.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
+          <Link className="navbar-brand" to="/">
+            Home
+          </Link>
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" href="register.html">
+                <Link className="nav-link" to="/register">
                   Sign Up
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="login.html">
+                <Link className="nav-link" to="/login">
                   Login
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
