dd2908ad3f0813175d7b6ccfe70e9743e894a941
Minh Phan <minhphan156@gmail.com>
2018-09-12 19:40:24 -0700

    remove asset, update types.js

--- front_end/src/actions/types.js
+++ front_end/src/actions/types.js
@@ -1,4 +1,2 @@
 export const GET_ERRORS = "GET_ERRORS";
-export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
-export const TEST_DISPATCH = "TEST_DISPATCH";
