9eb1eff4963bcd9f791aa92aec464b8b98043177
DaleCS <Dale.Seen@gmail.com>
2018-10-10 14:51:09 -0700

    Added alts to images

--- front_end/src/components/browse/BrowseByCategory.js
+++ front_end/src/components/browse/BrowseByCategory.js
@@ -12,21 +12,21 @@ class BrowseByCategory extends Component {
         <div className="row justify-content-md-center">
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Snacks_c.png" className="category-img" />
+              <img src="category-imgs/Snacks_c.png" className="category-img" alt="Snacks.img"/>
               <br />
               <div class="category-text">Snacks</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Drinks_c.png" className="category-img" />
+              <img src="category-imgs/Drinks_c.png" className="category-img" alt="Drinks.img"/>
               <br />
               <div class="category-text">Drinks</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Produce_c.png" className="category-img" />
+              <img src="category-imgs/Produce_c.png" className="category-img" alt="Produce.img"/>
               <br />
               <div class="category-text">Produce</div>
             </Link>
@@ -35,28 +35,28 @@ class BrowseByCategory extends Component {
         <div className="row justify-content-md-center">
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Bakery_c.png" className="category-img" />
+              <img src="category-imgs/Bakery_c.png" className="category-img" alt="Bakery.img"/>
               <br />
               <div class="category-text">Bakery</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Dairy_c.png" className="category-img" />
+              <img src="category-imgs/Dairy_c.png" className="category-img" alt="Dairy.img"/>
               <br />
               <div class="category-text">Dairy</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Meats_c.png" className="category-img" />
+              <img src="category-imgs/Meats_c.png" className="category-img" alt="Meats.img"/>
               <br />
               <div class="category-text">Meats</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Alcohol_c.png" className="category-img" />
+              <img src="category-imgs/Alcohol_c.png" className="category-img" alt="Alcohol.img"/>
               <br />
               <div class="category-text">Alcohol</div>
             </Link>
