374f89800f9b76d7ec3fd536266759faf091f6a5
Vinny Senthil <vinnysenthil@gmail.com>
2018-10-24 15:57:12 -0700

    Removing router.get search functions
    Search functions that were included before are not necessary in this PR and will be brought in during the Search feature PR

--- routes/api/product.js
+++ routes/api/product.js
@@ -7,32 +7,6 @@ const passport = require("passport");
 // Load Product model
 const Product = require("../../models/Product");
 
-// @route GET api/product/search
-// @desc Get array of products based on search query
-// @access Public
-// router.get("/search/:description", (req, res) => {
-router.get("/search/", (req, res) => {
-  // 3-receive req to query from submitQuery at /actions/queryActions.js
-  //NOTE:
-  // search/?string <-we use-> req.query
-  // search/:string <-we use-> req.params
-  const regex = new RegExp(req.query.name, "i"); // find part of string which include lower and upper case
-
-  // find return [ ]
-  Product.find({ name: regex })
-    .then(product => {
-      if (!product) {
-        // if product not found
-        errors = { query: "product not found" };
-        return res.status(404).json(errors);
-      }
-      res.send(product);
-    })
-    .catch(err => {
-      console.error(err);
-    });
-});
-
 // @route   GET api/product/:productname
 // @desc    Get product by name
 // @access  Public
