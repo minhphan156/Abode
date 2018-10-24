bcbbc05a56dc7a2e148ad83b2976909ffff46b1e
DaleCS <Dale.Seen@gmail.com>
2018-10-23 21:45:36 -0700

    Redid 'Browse by Category' page and 'ProductCard' component

--- front_end/src/components/browse/BrowseByCategory.js
+++ front_end/src/components/browse/BrowseByCategory.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 class BrowseByCategory extends Component {
   render() {
+    /*
     return (
       <div>
         <div className="text-center">
@@ -59,6 +60,121 @@ class BrowseByCategory extends Component {
               <img src="category-imgs/Alcohol_c.png" className="category-img" alt="Alcohol.img"/>
               <br />
               <div class="category-text">Alcohol</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+    */
+    return (
+      <div>
+        <h1 className="category-title text-center font-weight-bold">
+          Browse by Category
+        </h1>
+        <hr className="shadow" />
+        <div className="row justify-content-center">
+          <div className="col-md-3 pb-5">
+            <Link to="/categories">
+              <div className="border shadow m-0 p-0">
+                <img
+                  src="category-imgs/snacks.png"
+                  alt="Test Image"
+                  className="category-card-img img-fluid m-0 p-0"
+                />
+                <div className="category-card-nameBox text-center m-0 p-0">
+                  <span className="category-card-name align-middle m-0">
+                    Snacks
+                  </span>
+                </div>
+                <div className="category-card-botBox" />
+              </div>
+            </Link>
+          </div>
+          <div className="col-md-3 pb-5">
+            <Link to="/categories">
+              <div className="border shadow m-0 p-0">
+                <img
+                  src="category-imgs/drinks.png"
+                  alt="Test Image"
+                  className="category-card-img img-fluid m-0 p-0"
+                />
+                <div className="category-card-nameBox text-center m-0 p-0">
+                  <span className="category-card-name align-middle m-0">
+                    Drinks
+                  </span>
+                </div>
+                <div className="category-card-botBox" />
+              </div>
+            </Link>
+          </div>
+          <div className="col-md-3 pb-5">
+            <Link to="/categories">
+              <div className="border shadow m-0 p-0">
+                <img
+                  src="category-imgs/produce.jpg"
+                  alt="Test Image"
+                  className="category-card-img img-fluid m-0 p-0"
+                />
+                <div className="category-card-nameBox text-center m-0 p-0">
+                  <span className="category-card-name align-middle m-0">
+                    Produce
+                  </span>
+                </div>
+                <div className="category-card-botBox" />
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-3 pb-5">
+            <Link to="/categories">
+              <div className="border shadow m-0 p-0">
+                <img
+                  src="category-imgs/dairy.jpg"
+                  alt="Test Image"
+                  className="category-card-img img-fluid m-0 p-0"
+                />
+                <div className="category-card-nameBox text-center m-0 p-0">
+                  <span className="category-card-name align-middle m-0">
+                    Dairy
+                  </span>
+                </div>
+                <div className="category-card-botBox" />
+              </div>
+            </Link>
+          </div>
+          <div className="col-md-3 pb-5">
+            <Link to="/categories">
+              <div className="border shadow m-0 p-0">
+                <img
+                  src="category-imgs/meats.jpg"
+                  alt="Test Image"
+                  className="category-card-img img-fluid m-0 p-0"
+                />
+                <div className="category-card-nameBox text-center m-0 p-0">
+                  <span className="category-card-name align-middle m-0">
+                    Meats
+                  </span>
+                </div>
+                <div className="category-card-botBox" />
+              </div>
+            </Link>
+          </div>
+          <div className="col-md-3 pb-5">
+            <Link to="/categories">
+              <div className="border shadow m-0 p-0">
+                <img
+                  src="category-imgs/alcohol.jpg"
+                  alt="Test Image"
+                  className="category-card-img img-fluid m-0 p-0"
+                />
+                <div className="category-card-nameBox text-center m-0 p-0">
+                  <span className="category-card-name align-middle m-0">
+                    Alcohol
+                  </span>
+                </div>
+                <div className="category-card-botBox" />
+              </div>
             </Link>
           </div>
         </div>
