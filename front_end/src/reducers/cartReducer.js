ae0b33945fc1d8e6d08784382df2dcc29dfa20eb
Yu Ning Hee <yuninghee@gmail.com>
2018-11-01 02:22:08 -0700

    refactored REMOVE_ITEM for elegant solution

--- front_end/src/reducers/cartReducer.js
+++ front_end/src/reducers/cartReducer.js
@@ -8,16 +8,14 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case ADD_ITEM:
       for (var i = 0; i < state.shoppingCart.length; i++) {
-        if (state.shoppingCart[i] != undefined)
-          if (action.payload[0]._id == state.shoppingCart[i]._id) {
-            state.shoppingCart[i].count++;
-            return {
-              ...state,
-              shoppingCart: state.shoppingCart
-            };
-          }
+        if (action.payload[0]._id == state.shoppingCart[i]._id) {
+          state.shoppingCart[i].count++;
+          return {
+            ...state,
+            shoppingCart: state.shoppingCart
+          };
+        }
       }
-
       action.payload[0]["count"] = "1";
       return {
         ...state,
@@ -29,21 +27,18 @@ export default function(state = initialState, action) {
         state.shoppingCart = [];
       } else {
         for (var i = 0; i < state.shoppingCart.length; i++) {
-          if (state.shoppingCart[i] != undefined) {
-            if (action.payload.productId == state.shoppingCart[i]._id) {
-              state.shoppingCart[i].count--;
-              if (
-                state.shoppingCart[i].count == 0 ||
-                action.payload.removeAllItems
-              ) {
-                delete state.shoppingCart[i];
-              }
-              break;
+          if (action.payload.productId == state.shoppingCart[i]._id) {
+            state.shoppingCart[i].count--;
+            if (
+              state.shoppingCart[i].count == 0 ||
+              action.payload.removeAllItems
+            ) {
+              state.shoppingCart.splice(i, 1);
             }
+            break;
           }
         }
       }
-
       return {
         ...state,
         shoppingCart: state.shoppingCart
