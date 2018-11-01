0bb95e0c4651c1395499195c46ef3e736553174f
Yu Ning Hee <yuninghee@gmail.com>
2018-11-01 02:09:46 -0700

    removed bug in case ADD_ITEM, cleaned REMOVE_ITEM

--- front_end/src/reducers/cartReducer.js
+++ front_end/src/reducers/cartReducer.js
@@ -1,8 +1,7 @@
 import { ADD_ITEM, REMOVE_ITEM, INCREMENT_ITEM_COUNT } from "../actions/types";
 
 const initialState = {
-  shoppingCart: [], //initial shopping cart is empty
-  productIds: []
+  shoppingCart: [] //initial shopping cart is empty
 };
 
 export default function(state = initialState, action) {
@@ -22,24 +21,24 @@ export default function(state = initialState, action) {
       action.payload[0]["count"] = "1";
       return {
         ...state,
-        shoppingCart: [...state.shoppingCart, action.payload[0]],
-        productIds: [...state.productIds, action.payload[0]._id]
+        shoppingCart: [...state.shoppingCart, action.payload[0]]
       };
 
     case REMOVE_ITEM:
       if (action.payload.emptyCart) {
         state.shoppingCart = [];
-        state.productIds = [];
       } else {
-        for (var i = 0; i < state.productIds.length; i++) {
-          if (action.payload.productId == state.productIds[i]) {
-            state.shoppingCart[i].count--;
-            if (
-              state.shoppingCart[i].count == 0 ||
-              action.payload.removeAllItems
-            ) {
-              delete state.shoppingCart[i];
-              state.productIds[i] = "Removed Product ID";
+        for (var i = 0; i < state.shoppingCart.length; i++) {
+          if (state.shoppingCart[i] != undefined) {
+            if (action.payload.productId == state.shoppingCart[i]._id) {
+              state.shoppingCart[i].count--;
+              if (
+                state.shoppingCart[i].count == 0 ||
+                action.payload.removeAllItems
+              ) {
+                delete state.shoppingCart[i];
+              }
+              break;
             }
           }
         }
