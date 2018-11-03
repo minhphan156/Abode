8d0dab1f5a6f0c72a7bc9cd1000fbe73534c4e31
Yu Ning Hee <yuninghee@gmail.com>
2018-11-03 02:26:16 -0700

    Implemented checkout button on ShoppingCartModal in order for Modal to disappear once it redirects to CheckoutCart.js page

--- front_end/src/components/cart/ShoppingCartModal.js
+++ front_end/src/components/cart/ShoppingCartModal.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import { Modal } from "react-bootstrap";
 import ShoppingCart from "./ShoppingCart";
+import { Redirect } from "react-router";
 
 export default class ShoppingCartModal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      redirect: false
+    };
+  }
+
   render() {
+    if (this.state.redirect) {
+      this.state.redirect = false;
+      return <Redirect push to="/checkout" />;
+    }
     return (
       <Modal
         {...this.props}
@@ -22,6 +34,13 @@ export default class ShoppingCartModal extends Component {
         <Modal.Footer>
           <button className="btn" onClick={this.props.onHide}>
             Close
+          </button>
+          <button
+            onClick={() => this.setState({ redirect: true })}
+            className="btn btn-light"
+          >
+            <i className="fas fa-credit-card text-info mr-1" />
+            Checkout
           </button>
         </Modal.Footer>
       </Modal>
