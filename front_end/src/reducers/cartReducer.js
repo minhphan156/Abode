b0864cdda4c484b7d9fdf9e3ba9a25c89dd56159
Yu Ning Hee <yuninghee@gmail.com>
2018-11-03 11:36:26 -0700

    Added comments for cartReducer.js

--- front_end/src/reducers/cartReducer.js
+++ front_end/src/reducers/cartReducer.js
@@ -7,7 +7,9 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case ADD_ITEM:
+      //If there are already products in cart array, enter for loop
       for (var i = 0; i < state.shoppingCart.length; i++) {
+        //If there is an existing occurrence of this product being processed in cart
         if (action.payload[0]._id == state.shoppingCart[i]._id) {
           state.shoppingCart[i].count++;
           return {
@@ -16,7 +18,9 @@ export default function(state = initialState, action) {
           };
         }
       }
-      action.payload[0]["count"] = "1";
+      //If cart array empty OR there is NO occurrence of this product being
+      //processed, then this code gets executed
+      action.payload[0]["count"] = "1"; //Product object gets field count set to 1
       return {
         ...state,
         shoppingCart: [...state.shoppingCart, action.payload[0]]
@@ -24,11 +28,15 @@ export default function(state = initialState, action) {
 
     case REMOVE_ITEM:
       if (action.payload.productId == "All Product Ids") {
+        //delete products in shopping cart
         state.shoppingCart = [];
       } else {
         for (var i = 0; i < state.shoppingCart.length; i++) {
           if (action.payload.productId == state.shoppingCart[i]._id) {
+            //delete only one occurrence of product being processed
             state.shoppingCart[i].count--;
+            //If after deletion, count is 0, OR deleting of all occurrences
+            //of product being processed is requested, then remove Product object from cart
             if (
               state.shoppingCart[i].count == 0 ||
               action.payload.removeAllItems
@@ -45,6 +53,9 @@ export default function(state = initialState, action) {
       };
 
     case INCREMENT_ITEM_COUNT:
+      //More efficient implementation of incrementing item count as corresponding
+      //cart action function does not call back end, before sending payload INCREMENT_ITEM_COUNT
+      //to cart reducer
       for (let i = 0; i < state.shoppingCart.length; i++) {
         if (action.payload == state.shoppingCart[i]._id) {
           state.shoppingCart[i].count++;
