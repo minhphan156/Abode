aadb7d00c68ff9530dcd0684ffc0330be2a062cb
DaleCS <Dale.Seen@gmail.com>
2018-11-07 16:36:31 -0800

    Added delete/edit api for recipe backend

--- routes/api/recipe.js
+++ routes/api/recipe.js
@@ -31,7 +31,9 @@ router.post(
       .then(recipe => {
         // If there is already a recipe of the same title..
         if (recipe) {
-          return res.status(400).json({ title: "That title is already taken." });
+          return res
+            .status(400)
+            .json({ title: "That title is already taken." });
         } else {
           const newRecipe = new Recipe({
             userID: req.user.id,
@@ -39,7 +41,9 @@ router.post(
             title: req.body.title,
             date: Date(),
             description: req.body.description,
-            ingredients: req.body.ingredients
+            ingredients: req.body.ingredients,
+            likes: {},
+            unlikes: {}
           });
 
           newRecipe.save().then(recipe => res.json(recipe));
@@ -56,7 +60,7 @@ router.get("/", (req, res) => {
   Recipe.find()
     .sort({ date: -1 })
     .then(recipes => res.json(recipes))
-    .catch(err => res.status(404).json({ norecipesfound: 'No recipes found' }));
+    .catch(err => res.status(404).json({ norecipesfound: "No recipes found" }));
 });
 
 // @route   GET api/recipe/:id
@@ -67,33 +71,88 @@ router.get("/:id", (req, res) => {
     .then(recipe => {
       if (recipe) {
         return res.json(recipe);
-      }
-      else {
-        return res.status(404).json({ norecipefound: 'Such recipe does not exist' });
+      } else {
+        return res
+          .status(404)
+          .json({ norecipefound: "Such recipe does not exist" });
       }
     })
-    .catch(err => res.status(400).json({
-      norecipefound: 'Such recipe does not exist'
-    }));
+    .catch(err =>
+      res.status(400).json({
+        norecipefound: "Such recipe does not exist"
+      })
+    );
 });
 
 // @route DELETE api/recipe/:id
 // @desc  Delete a recipe made by the user who made it
 // @access Private
-router.delete("/:id", passport.authenticate("jwt", { session: false }), (req, res) => {
-  Profile.findOne({ user: req.user.id })
-    .then(profile => {
-      Recipe.findById(req.params.id)
-        .then(recipe => {
-          // Check for recipe owner
-          if (recipe.userID.toString() !== req.user.id) {
-            return res.status(401).json({ notauthorized: "User not authorized" });
-          }
-          recipe.remove().then(() => res.json({ success: true }));
-        })
-        .catch(err => res.status(404).json({ norecipefound: "Recipe doesn't exist" }));
-    })
-    .catch(err => res.status(401).json({ notauthorized: "User is not logged in" }));
-});
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Recipe.findById(req.params.id)
+          .then(recipe => {
+            // Check for recipe owner
+            if (recipe.userID.toString() !== req.user.id) {
+              return res
+                .status(401)
+                .json({ notauthorized: "User not authorized" });
+            }
+            recipe.remove().then(() => res.json({ success: true }));
+          })
+          .catch(err =>
+            res.status(404).json({ norecipefound: "Recipe doesn't exist" })
+          );
+      })
+      .catch(err =>
+        res.status(401).json({ notauthorized: "User is not logged in" })
+      );
+  }
+);
+
+//  @route  PUT api/recipe/:id
+//  @desc   Edits an existing recipe created by a user
+//  @access Private
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Recipe.findById(req.params.id)
+          .then(recipe => {
+            if (recipe) {
+              if (recipe.userID.toString() !== req.user.id) {
+                return res
+                  .status(401)
+                  .json({ notauthorized: "User not authorized " });
+              }
+
+              (recipe.title = req.body.title),
+                (recipe.description = req.body.description),
+                (recipe.ingredients = req.body.ingredients),
+                (recipe.date = Date());
+
+              recipe.save().then(recipe => res.json(recipe));
+
+              return res.json(recipe);
+            } else {
+              return res
+                .status(404)
+                .json({ norecipefound: "No such recipe exists" });
+            }
+          })
+          .catch(err =>
+            res.status(404).json({ norecipefound: "No such recipe exists" })
+          );
+      })
+      .catch(err =>
+        res.status(401).json({ notauthorized: "user must be logged in" })
+      );
+  }
+);
 
 module.exports = router;
