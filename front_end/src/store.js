a49b96f2be6e47531ad334019edd8603135e0f97
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2019-02-28 14:30:45 -0800

    fixed some small error

--- front_end/src/store.js
+++ front_end/src/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers"; // import reducers which is a function
-import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const initialState = {};
 
@@ -10,7 +9,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  devToolsEnhancer(
+  compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -4,3 +4,12 @@ const router = express.Router();
 const checkAvalibity = require("../../validation/checkAvalibility.js");
 const Hotel = require("../../models/Hotel");
 const Booking = require("../../models/booking");
+
+
+router.get('/',(req,res)=>{
+    console.log("hotle.js")
+})
+
+
+
+module.exports = router;
