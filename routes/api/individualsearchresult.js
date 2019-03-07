cae46a99b6e80e1ea2682093d75b78bce05be2ac
Tien Nguyen <tienquocnguyen22@gmail.com>
2019-03-06 17:41:02 -0800

    Changed to React format

--- routes/api/individualsearchresult.js
+++ routes/api/individualsearchresult.js
@@ -2,17 +2,11 @@ var express = require('express');
 var router = express.Router();
 var Hotel = require('../models/Hotel');
 
-router.get('/individual-search-result/:id', function (req, res, next){
-    var hotelId = req.params.id;
-    Hotel.findById(hotelId, function(err, hotel){
-        if (err){
-            return console.log("Hotel does not exist.");
-        }
-        return res.send({ name: hotel.name, hotelID: hotel._id, street: hotel.street, city: hotel.city, img: hotel.img, price: hotel.price, star_rating: hotel.star_rating, guest_rating: hotel.guest_rating,
-            guest_review: hotel.guest_review, amenities: hotel.amenities, airports: hotel.airports
-            //or res.json(hotel)
-        });
-    });
-});
+router.get('/individual-search-result/:id', (req,res) =>{
+    Hotel.findById(req.params.id)
+    //.then(hotel => res.send({ name: hotel.name, hotelID: hotel._id, street: hotel.street, city: hotel.city, img: hotel.img, price: hotel.price, star_rating: hotel.star_rating, guest_rating: hotel.guest_rating, guest_review: hotel.guest_review, amenities: hotel.amenities, airports: hotel.airports}))
+    .then(hotel => res.json(hotel))
+    .catch(err => res.status(404));
+})
 
 module.exports = router;
