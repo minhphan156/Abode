6b245b0e6f228c02ad11354dadac35d46e4462ea
Tien Nguyen <tienquocnguyen22@gmail.com>
2019-03-07 14:48:22 -0800

    final version

--- routes/api/individualsearchresult.js
+++ routes/api/individualsearchresult.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var Hotel = require('../models/Hotel');
+var Hotel = require('../../models/Hotel');
 
 router.get('/individual-search-result/:id', (req,res) =>{
     Hotel.findById(req.params.id)
-    //.then(hotel => res.send({ name: hotel.name, hotelID: hotel._id, street: hotel.street, city: hotel.city, img: hotel.img, price: hotel.price, star_rating: hotel.star_rating, guest_rating: hotel.guest_rating, guest_review: hotel.guest_review, amenities: hotel.amenities, airports: hotel.airports}))
-    .then(hotel => res.json(hotel))
+    .then(hotel => res.send({ name: hotel.name, hotelID: hotel._id, street: hotel.street, city: hotel.city, img: hotel.img, price: hotel.price, star_rating: hotel.star_rating, guest_rating: hotel.guest_rating, guest_review: hotel.guest_review, amenities: hotel.amenities, airports: hotel.airports}))
     .catch(err => res.status(404));
 })
 
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const mongoose = require("mongoose");
 // api routes
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
+const hotel = require("./routes/api/individualsearchresult");
 
 // get some functionalities from express library like get() function
 const app = express();
@@ -42,6 +43,8 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 // this will append to home route 'localHost:5000/api/profile/{what ever profile.js dictate}'
 app.use("/api/profile", profile);
+// individual search result route
+app.use("/api/individualsearchresult", hotel);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
