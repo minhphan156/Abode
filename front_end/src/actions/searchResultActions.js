a73d715b9a2011b455bf6c78b272da0f6fec742c
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2019-03-16 21:58:30 -0700

    fix the maps

--- front_end/src/actions/searchResultActions.js
+++ front_end/src/actions/searchResultActions.js
@@ -1,14 +1,29 @@
 import { GET_INDIVIDUAL_HOTEL } from "./types";
 import axios from "axios";
+import Geocode from "react-geocode";
+
+Geocode.setApiKey("AIzaSyDW-Gy3YtzwfsT2pstjlMU2Q5U4TjRJZp8");
 
 export const getIndividualHotelResult = hotel => dispatch => {
   axios
     .get("/api/hotel/individual", { params: hotel })
     .then(res => {
-      dispatch({
-        type: GET_INDIVIDUAL_HOTEL,
-        payload: res.data
-      });
+      var hotelInfo = res.data
+      Geocode.fromAddress(hotelInfo.street + hotelInfo.city).then(
+        response => {
+          const { lat, lng } = response.results[0].geometry.location;
+          hotelInfo.lat = lat;
+          hotelInfo.alt = lng
+          console.log(hotelInfo)
+          dispatch({
+            type: GET_INDIVIDUAL_HOTEL,
+            payload: hotelInfo
+          });
+        },
+        error => {
+          console.error(error);
+        }
+      );
     })
     .catch(err => console.log(err));
 };
