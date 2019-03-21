e426f1f474d7ef30652f82e7d8418b73514691f7
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2019-03-21 15:33:49 -0700

    fix error

--- routes/api/landing.js
+++ routes/api/landing.js
@@ -3,17 +3,31 @@ const router = express.Router();
 
 const City = require("../../models/city");
 
-router.get("/", function(req, res) {
-  City.find({}, function(err, doc){
-      if(err) res.status(400).json(err);
-      var destinations = [];
-      var i;
-      //change loop limit if want more featured cities
-      for(i = 0; i < 5; i++){
-          let a = Math.floor(Math.random() * doc.length);
-          destinations.push(doc[a]);
-      }
-      res.send(destinations);
+router.get("/", function (req, res) {
+  City.find({}, function (err, doc) {
+    if (err) res.status(400).json(err);
+    var destinations = [];
+    var i;
+    //change loop limit if want more featured cities
+    var index = Math.floor(Math.random() * doc.length)
+    console.log(doc[index])
+    var headImg = doc[index].imgMain
+    var headCityName = doc[index].name
+    for (i = 0; i < 5; i++) {
+      let a = Math.floor(Math.random() * doc.length);
+
+      destinations.push({cityName:doc[a].name,
+                            picurl:doc[a].imgMain});
+    }
+    const result = {
+      header: {
+        cityName: headCityName,
+        headImg: headImg
+      },
+      featureDestination: destinations
+    }
+    //console.log(result)
+    res.send(result);
      
       /* **If repeating cities is concern, use shuffle **
       doc = shuffle(doc);
