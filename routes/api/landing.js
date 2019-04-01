0347e2ee6ca424a0ebd11f12449dfdb2a28aeddb
lancey <qvbingo@gmail.com>
2019-04-01 16:05:06 -0700

    modified city to include inspire and bookings

--- routes/api/landing.js
+++ routes/api/landing.js
@@ -4,47 +4,52 @@ const router = express.Router();
 const City = require("../../models/city");
 
 router.get("/", function(req, res) {
-  City.find({}, function(err, doc){
-      if(err) res.status(400).json(err);
-      var destinations = [];
-      var i;
-      //change loop limit if want more featured cities
-      for(i = 0; i < 5; i++){
-          let a = Math.floor(Math.random() * doc.length);
-          destinations.push(doc[a]);
-      }
-      res.send(destinations);
-     
-      /* **If repeating cities is concern, use shuffle **
+  City.find({}, function(err, doc) {
+    if (err) res.status(400).json(err);
+
+    var landingInfo = {
+      inspire: "",
+      inspireCity: "",
+      featCities: []
+    };
+    var i;
+    doc.sort((a, b) => a.bookings - b.bookings);
+    let a = Math.floor(Math.random() * doc.length);
+    landingInfo.inspire = doc[a].inspire;
+    landingInfo.inspireCity = doc[a].name;
+
+    //change loop limit if want more featured cities
+    for (i = 0; i < 4; i++) {
+      landingInfo.featCities.push(doc[a]);
+    }
+    res.send(landingInfo);
+
+    /* **If repeating cities is concern, use shuffle **
       doc = shuffle(doc);
       for(i = 0; i < 5; i++){
           destinations.push(doc.pop());
       }
       res.send(destinations);
       */
-    })
-    
+  });
 
   //fisher yates shuffle of cities
-function shuffle(arr) {
+  function shuffle(arr) {
     var curIndex = arr.length;
     var ranIndex;
     var tempValue;
-  
+
     while (curIndex !== 0) {
       ranIndex = Math.floor(Math.random() * curIndex);
       curIndex -= 1;
-  
+
       tempValue = arr[curIndex];
       arr[curIndex] = arr[ranIndex];
       arr[ranIndex] = tempValue;
     }
-  
+
     return arr;
   }
-
 });
-
-
 
 module.exports = router;
