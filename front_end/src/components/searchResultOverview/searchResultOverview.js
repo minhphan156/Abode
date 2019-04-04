6ff1ecfca7124af35c45d9319904d8826223736b
DaleCS <Dale.Seen@gmail.com>
2019-04-03 23:01:12 -0700

    Attempt at Redux

--- front_end/src/components/searchResultOverview/searchResultOverview.js
+++ front_end/src/components/searchResultOverview/searchResultOverview.js
@@ -12,7 +12,7 @@ import SearchWidget from "../landing_page/search_widget/SearchWidget";
 import FiltersWindow from "./FiltersWindow.js";
 import SortBar from "./SortBar.js";
 
-// Test imports
+// PROTOTYPE IMPORTS
 import LV from "./hotel-img-5.jpg";
 
 // Material UI Imports
@@ -78,6 +78,7 @@ class searchResultOverview extends Component {
     this.handleGuestRatings = this.handleGuestRatings.bind(this);
     this.handleAmenities = this.handleAmenities.bind(this);
     this.handlePriceRangeChange = this.handlePriceRangeChange.bind(this);
+    this.handleClickToHotel = this.handleClickToHotel.bind(this);
   }
 
   // Used to force user browser to scroll to top of page upon mounting this component
@@ -122,10 +123,23 @@ class searchResultOverview extends Component {
     });
   };
 
+  handleClickToHotel = hotel => event => {
+    event.preventDefault();
+    /*
+    // TODO: Update action call upon merging with backend
+    this.props.getIndividualHotelResult({
+      id: hotel.hotelID,
+      checkIn: searchQuery.checkIn,
+      checkOut: searchQuery.checkOut,
+      numberRooms: searchQuery.numberRooms
+    });
+    */
+  };
+
   render() {
     let { star_rate, guest_rate, price_low, price_high } = this.state;
     let { classes, width } = this.props;
-    let { hotelQuery, searchQuery } = this.props.query;
+    let { hotelQuery } = this.props.query;
 
     let hotels;
     if (hotelQuery.length > 0) {
@@ -214,7 +228,11 @@ class searchResultOverview extends Component {
                   </Grid>
                 </Grid>
                 <Grid item xs="auto">
-                  <Button variant="contained" color="primary">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={this.handleClickToHotel(hotel)}
+                  >
                     Book Now
                   </Button>
                 </Grid>
@@ -223,21 +241,7 @@ class searchResultOverview extends Component {
           </Grid>
         );
       });
-    } else {
-      hotels = <CircularProgress color="primary" />;
     }
-
-    /*
-      onClick={event => {
-        event.preventDefault();
-        this.props.getIndividualHotelResult({
-          id: hotel.hotelID,
-          checkIn: searchQuery.checkIn,
-          checkOut: searchQuery.checkOut,
-          numberRooms: searchQuery.numberRooms
-        });
-      }}
-    */
 
     let pagination = (
       <Grid item>
@@ -253,7 +257,23 @@ class searchResultOverview extends Component {
 
     return (
       <div className={classes.pageMargins}>
-        <SearchWidget />
+        <SearchWidget
+          sortObject={{
+            sortCategory: this.state.sortCategory,
+            sortOrder: this.state.sortOrder
+          }}
+          filterObject={{
+            star_rate: this.state.star_rate,
+            guest_rate: this.state.guest_rate,
+            price_low: this.state.price_low,
+            price_high: this.state.price_high,
+            free_wifi: this.state.free_wifi,
+            free_parking: this.state.free_parking,
+            free_breakfast: this.state.free_breakfast,
+            pool: this.state.pool,
+            pet_friendly: this.state.pet_friendly
+          }}
+        />
         <Grid container direction="flow" spacing={8}>
           <FiltersWindow
             star_rate={star_rate}
@@ -302,91 +322,3 @@ export default connect(
   mapStateToProps,
   { getIndividualHotelResult }
 )(withStyles(styles)(withWidth()(searchResultOverview)));
-
-/*
-<Grid item>
-  <Card style={{ padding: 25 }} square="false">
-    <Grid
-      container
-      direction="flow"
-      justify={isWidthDown("sm", width) ? "center" : "flex-start"}
-      alignItems="center"
-      spacing={8}
-    >
-      <Grid item xs={12} md>
-        <Grid container direction="column" spacing={0}>
-          <Grid item>
-            <Typography variant="h5" color="primary">
-              Mandalay Bay Resort And Casino
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="subtitle1" color="secondary">
-              Las Vegas, Nevada
-            </Typography>
-          </Grid>
-          <Grid item style={{ paddingTop: 8 }}>
-            <Grid container direction="flow" spacing={16}>
-              <Grid item xs={12} md={4} lg={3}>
-                <Card style={{ padding: 7 }}>
-                  <img src={LV} />
-                </Card>
-              </Grid>
-              <Grid item xs={12} md="auto">
-                <Grid
-                  container
-                  direction="column"
-                  justify={
-                    isWidthDown("sm", width) ? "center" : "flex-start"
-                  }
-                  alignItems={
-                    isWidthDown("sm", width) ? "center" : "flex-start"
-                  }
-                  spacing={0}
-                >
-                  <Grid item xs={12} md="auto">
-                    <Grid
-                      container
-                      direction="flow"
-                      justify={
-                        isWidthDown("sm", width) ? "center" : "flex-start"
-                      }
-                      alignItems="center"
-                      spacing={8}
-                    >
-                      <Grid item>
-                        <ReactStars
-                          value={4}
-                          count={5}
-                          size={32}
-                          color2={"#ffd700"}
-                          edit={false}
-                        />
-                      </Grid>
-                      <Grid item>
-                        <Typography variant="subtitle2" color="default">
-                          4 out of 5 Star Rating
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h6" color="default">
-                      Fabulous! 8.9 Guest Rating
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs="auto">
-        <Button variant="contained" color="primary">
-          Book Now
-        </Button>
-      </Grid>
-    </Grid>
-  </Card>
-</Grid>
-*/
