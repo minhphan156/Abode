44be05c1eaff41509c0818d4219132e53fe621e9
Minh Phan <minhphan156@gmail.com>
2019-04-04 17:59:04 -0700

    checkout branch

---
+++ front_end/src/reducers/bookingReducer.js
@@ -0,0 +1,36 @@
+import { SAVE_BOOKING } from "../actions/types";
+
+const initialState = {
+  bookingId: "123241",
+  hotelName: "Hampton Inn San Francisco Downtown/Convention Center",
+  hotelAddress: "942 Mission Street San Francisco, CA, 94103, United States",
+  hotelImg: "",
+  destinationName: "",
+  destinationImg: null,
+  checkIn: "4/2/2019",
+  checkOut: "4/6/2019",
+  numRooms: 2,
+  roomType: "king",
+  Firstname: "Minh",
+  Lastname: "Phan",
+  email: "minh156@gmail.com",
+  subtotal: 123,
+  discounts: 22,
+  hotelImage: "",
+  rewardPointsUsed: 100,
+  rewardPointsEarned: 1000,
+  reservedDate: ""
+};
+
+// ...state = current state
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SAVE_BOOKING:
+      return {
+        ...state,
+        bookingData: action.payload
+      };
+    default:
+      return state;
+  }
+}
