397a871ee1a4a61cb948abc9049619db970212cb
Sandro Sallenbach <hcl.sandro@hotmail.de>
2019-04-04 22:10:51 -0700

    made aboutUs and Search Widget mobile friendly

--- front_end/src/components/landing_page/search_widget/SearchWidget.js
+++ front_end/src/components/landing_page/search_widget/SearchWidget.js
@@ -20,8 +20,7 @@ const styles = theme => ({
   },
   paper: {
     textAlign: "center",
-    height: 70,
-    margin: 10
+    height: 70
   }
 });
 
@@ -103,7 +102,7 @@ class SearchWidget extends Component {
               />
             </Paper>
           </Grid>
-          <Grid item xs={3} sm={3} md={4} lg={1}>
+          <Grid item xs={3} sm={3} md={2} lg={1}>
             <Paper className={classes.paper}>
               <RoomNumber
                 numberRooms={this.state.numberRooms}
