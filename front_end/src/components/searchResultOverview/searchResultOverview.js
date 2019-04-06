083dd42bab1bef44cee6309d9af0a9d24dfc9916
Sandro Sallenbach <hcl.sandro@hotmail.de>
2019-04-05 21:05:23 -0700

    little fix

--- front_end/src/components/searchResultOverview/searchResultOverview.js
+++ front_end/src/components/searchResultOverview/searchResultOverview.js
@@ -1,319 +1,219 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getIndividualHotelResult } from "../../actions/searchResultActions";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-
-// Additional libraries
-import ReactStars from "react-stars";
-
-// Child Component Imports
-import SearchWidget from "../landing_page/search_widget/SearchWidget";
-import FiltersWindow from "./FiltersWindow.js";
-import SortBar from "./SortBar.js";
-
-// PROTOTYPE IMPORTS
-import LV from "./hotel-img-5.jpg";
-
-// Material UI Imports
 import {
-  withStyles,
-  withWidth,
   Grid,
   Card,
-  Button,
+  CardContent,
+  CardMedia,
   Typography,
+  Checkbox,
+  FormControlLabel,
   TablePagination,
-  CircularProgress
+  CardHeader
 } from "@material-ui/core";
-import { isWidthDown } from "@material-ui/core/withWidth";
+import SearchWidget from "../landing_page/search_widget/SearchWidget";
+import { connect } from "react-redux";
+import { getIndividualHotelResult } from "../../actions/searchResultActions";
+import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import ReactStars from "react-stars";
 
-// Component CSS to Javascript styles
-let styles = theme => ({
-  pageMargins: {
-    // Mobile margins
-    [theme.breakpoints.down("sm")]: {
-      marginLeft: "5%",
-      marginRight: "5%",
-      marginTop: "2%",
-      marginBottom: "2%"
-    },
-    // Tablet, laptop, desktop margins
-    [theme.breakpoints.up("md")]: {
-      marginLeft: "10%",
-      marginRight: "10%",
-      marginTop: "1%",
-      marginBottom: "1%"
-    }
-  },
-  subtitles: { fontWeight: "bold", color: "#808080" },
-  pad25: { padding: 25 }
-});
+const styles = {
+  rating: { float: "left", width: "100%" },
+  imageStyle: { margin: 20, width: 200, height: 200, float: "left" }
+};
 
-// Main component of the Search Result Overview Page
 class searchResultOverview extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      // States used for sorting
-      sortCategory: "name",
-      sortOrder: "descending",
-
-      // States used for filter
-      star_rate: 0,
-      guest_rate: 0,
-      price_low: 0,
-      price_high: 0,
-      free_wifi: false,
-      free_parking: false,
-      free_breakfast: false,
-      pool: false,
-      pet_friendly: false,
-
-      // State used for pagination
-      page: 0
-    };
-
-    // Methods passed to child components
-    this.handleChange = this.handleChange.bind(this);
-    this.handleStarRatings = this.handleStarRatings.bind(this);
-    this.handleGuestRatings = this.handleGuestRatings.bind(this);
-    this.handleAmenities = this.handleAmenities.bind(this);
-    this.handlePriceRangeChange = this.handlePriceRangeChange.bind(this);
-    this.handleClickToHotel = this.handleClickToHotel.bind(this);
-  }
-
-  // Used to force user browser to scroll to top of page upon mounting this component
   componentDidMount = () => {
     window.scrollTo(0, 0);
   };
 
-  // Used to store the star rating input in <FiltersWindow />
-  handleStarRatings = newRating => {
-    this.setState({
-      star_rate: newRating
-    });
-  };
-
-  // Used to store the star rating input in <FiltersWindow />
-  handleGuestRatings = (event, value) => {
-    event.preventDefault();
-    this.setState({
-      guest_rate: value
-    });
-  };
-
-  // Used to store the amenities input in <FiltersWindow />
-  handleAmenities = event => {
-    event.preventDefault();
-    this.setState({
-      [event.target.name]: event.target.checked
-    });
-  };
-
-  // Used to store the inputs from price range fields in <FiltersWindow />
-  handlePriceRangeChange = name => event => {
-    this.setState({
-      [name]: Number(event.target.value)
-    });
-  };
-
-  // Used to store the inputs from selections felds in <SortBar />
-  handleChange = event => {
-    event.preventDefault();
-    this.setState({
-      [event.target.name]: [event.target.value]
-    });
-  };
-
-  handleClickToHotel = hotel => event => {
-    event.preventDefault();
-    /*
-    // TODO: Update action call upon merging with backend
-    this.props.getIndividualHotelResult({
-      id: hotel.hotelID,
-      checkIn: searchQuery.checkIn,
-      checkOut: searchQuery.checkOut,
-      numberRooms: searchQuery.numberRooms
-    });
-    */
-  };
-
   render() {
-    let { star_rate, guest_rate, price_low, price_high, page } = this.state;
-    let { classes, width } = this.props;
-    let { hotelQuery } = this.props.query;
-
+    let { classes } = this.props;
     let hotels;
-    if (hotelQuery.length > 0) {
-      hotels = hotelQuery.map(hotel => {
-        return(
-          <Grid item>
-            <Card className={classes.pad25} square="false">
-              <Grid
-                container
-                direction="flow"
-                justify={isWidthDown("sm", width) ? "center" : "flex-start"}
-                spacing={8}
-              >
-                <Grid item xs={12} md>
-                  <Grid container direction="column" spacing={0}>
-                    <Grid item>
-                      <Typography variant="h5" color="primary">
-                        Mandalay Bay Resort And Casino
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <Typography variant="subtitle1" color="secondary">
-                        Las Vegas, Nevada
-                      </Typography>
-                    </Grid>
-                    <Grid item style={{ paddingTop: 8 }}>
-                      <Grid container direction="flow" spacing={16}>
-                        <Grid item xs={12} md={4} lg={3}>
-                          <Card style={{ padding: 7 }}>
-                            <img src={LV} />
-                          </Card>
-                        </Grid>
-                        <Grid item xs={12} md="auto">
-                          <Grid
-                            container
-                            direction="column"
-                            justify={
-                              isWidthDown("sm", width) ? "center" : "flex-start"
-                            }
-                            alignItems={
-                              isWidthDown("sm", width) ? "center" : "flex-start"
-                            }
-                            spacing={0}
-                          >
-                            <Grid item xs={12} md="auto">
-                              <Grid
-                                container
-                                direction="flow"
-                                justify={
-                                  isWidthDown("sm", width)
-                                    ? "center"
-                                    : "flex-start"
-                                }
-                                alignItems="center"
-                                spacing={8}
-                              >
-                                <Grid item>
-                                  <ReactStars
-                                    value={4}
-                                    count={5}
-                                    size={32}
-                                    color2={"#ffd700"}
-                                    edit={false}
-                                  />
-                                </Grid>
-                                <Grid item>
-                                  <Typography
-                                    variant="subtitle2"
-                                    color="default"
-                                  >
-                                    4 out of 5 Star Rating
-                                  </Typography>
-                                </Grid>
-                              </Grid>
-                            </Grid>
-                            <Grid item>
-                              <Typography variant="h6" color="default">
-                                Fabulous! 8.9 Guest Rating
-                              </Typography>
-                            </Grid>
-                          </Grid>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <Grid item>
-                  <Grid container direction="column" justify="space-between" alignItems="center">
-                    <Grid item xs="auto">
-                      <Typography variant="h5">
-                        $500
-                      </Typography>
-                    </Grid>
-                    <Grid item xs="auto">
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={this.handleClickToHotel(hotels)}
+    const queryResult = this.props.query.hotelQuery;
+    const searchQuery = this.props.query.searchQuery;
+    if (queryResult.length) {
+      hotels = queryResult.map(hotel => {
+        return (
+          <Card style={{ marginBottom: 10 }}>
+            <CardContent>
+              <div>
+                <Link
+                  to="/indiv-hotel"
+                  onClick={() =>
+                    this.props.getIndividualHotelResult({
+                      id: hotel.hotelID,
+                      checkIn: searchQuery.checkIn,
+                      checkOut: searchQuery.checkOut,
+                      numberRooms: searchQuery.numberRooms
+                    })
+                  }
+                >
+                  <div className="row">
+                    <div className="col-10" style={{ align: "left" }}>
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="h2"
+                        style={{ display: "inline" }}
                       >
-                        Book Now
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
+                        {hotel.name}
+                      </Typography>
+                    </div>
+                    <div className="col-2" style={{ align: "right" }}>
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="h5"
+                        style={{ color: "green", display: "inline" }}
+                        align="right"
+                      >
+                        ${hotel.price}
+                      </Typography>
+                    </div>
+                  </div>
+                </Link>
+
+                <Typography
+                  style={{
+                    color: "#808080",
+                    marginLeft: "1%",
+                    marginTop: "1%"
+                  }}
+                  component="h3"
+                >
+                  {hotel.address}
+                </Typography>
+              </div>
+            </CardContent>
+            <div style={{ float: "left" }}>
+              <CardMedia
+                className={classes.imageStyle}
+                image={`${hotel.img}`}
+                title="Hotel Image"
+              />
+            </div>
+            <div style={{ float: "right" }}>
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="h2"
+                  style={{ marginRight: "0" }}
+                >
+                  <h5>{hotel.star_rates}-Star Hotel</h5>
+                  <ReactStars
+                    count={5}
+                    value={hotel.star_rates}
+                    size={22}
+                    edit={false}
+                    color2={"#FFD700"}
+                    style={{ align: "right" }}
+                  />
+                </Typography>
+
+                <Typography variant="h5" component="h5">
+                  Guest Rating: {hotel.guest_rate}
+                </Typography>
+
+                <Link
+                  to="/indiv-hotel"
+                  onClick={() =>
+                    this.props.getIndividualHotelResult({
+                      id: hotel.hotelID,
+                      checkIn: searchQuery.checkIn,
+                      checkOut: searchQuery.checkOut,
+                      numberRooms: searchQuery.numberRooms
+                    })
+                  }
+                >
+                  <button type="button" class="btn btn-success h-100">
+                    Book Now
+                  </button>
+                </Link>
+              </CardContent>
+            </div>
+          </Card>
+        );
+      });
+    }
+    return (
+      <div>
+        <Grid container spacing={24} justify="center">
+          <Grid item xs={10} style={{ marginLeft: 30 }}>
+            <SearchWidget />
+          </Grid>
+          <Grid item xs={2} style={{ marginLeft: 30 }}>
+            <Card style={{ marginBottom: 10 }}>
+              <div style={{ margin: 20 }}>
+                <Typography>Star Rating</Typography>
+                <FormControlLabel
+                  control={<Checkbox value="star1" />}
+                  label="1 star"
+                  className={classes.rating}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="star2" />}
+                  label="2 stars"
+                  className={classes.rating}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="star3" />}
+                  label="3 stars"
+                  className={classes.rating}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="star4" />}
+                  label="4 stars"
+                  className={classes.rating}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="star5" />}
+                  label="5 stars"
+                  className={classes.rating}
+                />
+              </div>
+            </Card>
+            <Card style={{ marginBottom: 10 }}>
+              <div style={{ margin: 20 }}>
+                <Typography>Review Score</Typography>
+                <FormControlLabel
+                  control={<Checkbox value="Awesome" />}
+                  label="Awesome: 9+"
+                  className={classes.rating}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="Verygood" />}
+                  label="Very good: 8+"
+                  className={classes.rating}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="Good" />}
+                  label="Good: 7+"
+                  className={classes.rating}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="Pleasant" />}
+                  label="Pleasant: 6+"
+                  className={classes.rating}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="NoRating" />}
+                  label="No rating"
+                  className={classes.rating}
+                />
+              </div>
             </Card>
           </Grid>
-        );
-      })
-    }
-
-    let pagination = (
-      <Grid item>
-        <TablePagination
-          style={{ float: "right" }}
-          rowsPerPageOptions={[5, 10, 25]}
-          count={100}
-          page={page}
-          rowsPerPage={10}
-        />
-      </Grid>
-    );
-
-    return (
-      <div className={classes.pageMargins}>
-        <SearchWidget
-          sortObject={{
-            sortCategory: this.state.sortCategory,
-            sortOrder: this.state.sortOrder
-          }}
-          filterObject={{
-            star_rate: this.state.star_rate,
-            guest_rate: this.state.guest_rate,
-            price_low: this.state.price_low,
-            price_high: this.state.price_high,
-            free_wifi: this.state.free_wifi,
-            free_parking: this.state.free_parking,
-            free_breakfast: this.state.free_breakfast,
-            pool: this.state.pool,
-            pet_friendly: this.state.pet_friendly
-          }}
-        />
-        <Grid container direction="flow" spacing={8}>
-          <FiltersWindow
-            star_rate={star_rate}
-            guest_rate={guest_rate}
-            price_low={price_low}
-            price_high={price_high}
-            handleAmenities={this.handleAmenities}
-            handleStarRatings={this.handleStarRatings}
-            handleGuestRatings={this.handleGuestRatings}
-            handlePriceRangeChange={this.handlePriceRangeChange}
-          />
-          <Grid item xs={12} sm={8} md={9} lg={10}>
-            <Grid container direction="flow" justify="center" spacing={8}>
-              <Grid item xs={12}>
-                <SortBar
-                  sortCategory={this.state.sortCategory}
-                  sortOrder={this.state.sortOrder}
-                  handleChange={this.handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <Grid container spacing={8}>
-                  {/* TODO: Show Spinner when fetching from backend*/}
-                  {hotels}
-                </Grid>
-              </Grid>
-            </Grid>
-            {hotelQuery.length ? pagination : <div />}
+          <Grid item xs={7}>
+            {hotels}
+            <TablePagination
+              style={{ float: "right" }}
+              rowsPerPageOptions={[5, 10, 25]}
+              count={100}
+              page={9}
+              rowsPerPage={10}
+            />
           </Grid>
         </Grid>
       </div>
@@ -321,17 +221,10 @@ class searchResultOverview extends Component {
   }
 }
 
-searchResultOverview.propTypes = {
-  query: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired,
-  width: PropTypes.func.isRequired
-};
-
 const mapStateToProps = state => ({
   query: state.query
 });
-
 export default connect(
   mapStateToProps,
   { getIndividualHotelResult }
-)(withStyles(styles)(withWidth()(searchResultOverview)));
+)(withStyles(styles)(searchResultOverview));
