afa70d1322dee85701c188a6bac0105ad3b31fd0
Vinny <vinnysenthil@gmail.com>
2019-04-10 12:07:37 -0700

    Both routes working now

--- routes/api/bookin.js
+++ routes/api/bookin.js
@@ -12,58 +12,54 @@ const checkAvalibity = require("../../validation/checkAvailableHotels");
 
 // @route GET /api/booking/history
 // @desc History page
-// @access public
+// @access private
 router.get("/history", (req, res) => {
     const userEmail = req.body.email;
     var historyPack = [];
 
-    User.findOne({email: userEmail}).then((err, doc) => {
-        if(err) res.status(400).json(err)
+        // Get the User being requested
+        User.findOne({email: userEmail}, (err, doc) => {
+            if(err) return res.status(400).json(err)
 
-        // Uncomment below during production
-        // idToSearch = doc.customerID;
+            // Get all the Bookings that User has made
+            Booking.find({customerID: doc.customerID}, (err, bookings) => {
+                if(err) return res.status(400).json(err);
 
-        // Searching with a Customer ID of a User that has a booking
-        idToSearch = "5ca03969306f0952106d6eed";
+                    // Get the Hotel info of each of their Bookings
+                    bookings.map((element, i) => { 
 
-        Booking.find({customerID: idToSearch}).then((err, bookings) => {
-            if(err) res.status(400).json(err)
+                        Hotel.findById(element.hotelID, (err, hotelDoc) => {
+                            if(err) return res.status(400).json(err)
 
-            bookings.forEach((elements) => { 
+                            // Add the hotel details and relevant booking details as a history object
+                            historyPack.push(
+                            {
+                                hotelName: hotelDoc.name,
+                                img: hotelDoc.images[0],
+                                city: hotelDoc.city,
+                                check_in_date: element.check_in_date,
+                                check_out_date: element.check_out_date,
+                                typeOfRoom: element.typeOfRoom,
+                                numOfRoom: element.numOfRoom,
+                                status: element.status,
+                                changed: element.changed,
+                                new_check_in_date: element.new_check_in_date,
+                                new_check_out_date: element.new_check_out_date,
+                                subtotal: element.subtotal,
+                                discount: element.discount
+                            });
 
-                Hotel.findById(elements.hotelID).then((err, hotelDoc) => {
-                    if(err) res.status(400).json(err)
+                            // Check if we've finished packing all the history objects
+                            if (i === (bookings.length - 1)) return res.status(200).send(historyPack);
 
-                    historyPack.push(
-                    {
-                        hotelName: hotelDoc.name,
-                        img: hotelDoc.images[0],
-                        city: hotelDoc.city,
-                        check_in_date: elements.check_in_date,
-                        check_out_date: elements.check_out_date,
-                        typeOfRoom: elements.typeOfRoom,
-                        numOfRoom: elements.numOfRoom,
-                        status: elements.status,
-                        changed: elements.changed,
-                        new_check_in_date: elements.new_check_in_date,
-                        new_check_out_date: elements.new_check_out_date,
-                        subtotal: elements.subtotal,
-                        discount: elements.discount
-                    });    
+                        });
 
-                }).then(() => {
+                    }); // end map()
 
-                    // After finding the Hotel info of every booking,
-                    // Give frontend the goods
-                    res.send(historyPack)
+            }); // end Booking.find
 
-                });
-            }); 
+        }); // end User.findOne()
         
-        });
-    
-    });
-
 });
 
 // @route GET /api/booking/history
@@ -72,19 +68,17 @@ router.get("/history", (req, res) => {
 router.get("/guest-history", (req, res) => {
     const bookingID = req.body.bookingID;
     const lastName = req.body.lastName;
-    const errMessage = "Wrong Last Name";
     
     var historyPack = {};
 
-    Booking.findById(bookingID).then((book,err) => {
-        if(err) res.status(400).json(err)
+    Booking.findById(bookingID, (err, book) => {
+        if(err) return res.status(404).json(err);
 
-        Customer.findById(book.customerID, (err, custDoc) => {
-            if(err) res.status(400).json(err);
+        Customer.findById(book.customerID).then((custDoc) => {
 
             // Block request if the last name does not match the booking
-            else if(custDoc.Lastname !== lastName) res.status(403).json({
-                err: errMessage
+            if(custDoc.Lastname !== lastName) return res.status(403).json({
+                err: true
             })
 
             // User checking history has been authorized
@@ -99,21 +93,24 @@ router.get("/guest-history", (req, res) => {
             historyPack.new_check_out_date = book.new_check_out_date;
             historyPack.subtotal = book.subtotal;
             historyPack.discount = book.discount;
+            historyPack.err = false;
 
-
-        }).then(() => {
-            Hotel.findById(book.hotelID, (err, hotelDoc) => {
-                if(err) res.status(400).json(err);
-
+            Hotel.findById(book.hotelID).then(hotelDoc => {
                 // Get the name, city and image of the hotel the User is staying at
                 historyPack.hotelName = hotelDoc.name;
                 historyPack.img = hotelDoc.images[0];
                 historyPack.city = hotelDoc.city;
 
                 // Give frontend the goods
-                res.send(historyPack)
+                return res.status(200).send(historyPack);
             });
+
+        }).catch((err) => {
+            
+            if(err) return res.status(404).json(err);
+
         });
+
     });
 });
 
