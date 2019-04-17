9037255a5e69c94be66454c04738983a1f6bbdb0
Minh Phan <minhphan156@gmail.com>
2019-04-17 15:49:37 -0700

    need to hookup with backend

--- front_end/src/actions/reviewActions.js
+++ front_end/src/actions/reviewActions.js
@@ -1,6 +1,7 @@
 import { PUBLISH_REVIEW } from "./types";
 
 export const submitReview = reviewData => {
+  // todo: need to call backend endpoint to update star rating and comments
   return {
     type: PUBLISH_REVIEW,
     payload: reviewData
