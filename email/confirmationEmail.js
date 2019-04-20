91a5767ba2184d7c1244bac9a26add7d34f9034c
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2019-04-20 00:20:36 -0700

    email style change

--- email/confirmationEmail.js
+++ email/confirmationEmail.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-const keys = require('../config/keys')
+const keys = require('../config/keys');
 
 module.exports = function confirmEmail(firstname,lastname,id,hotelName,typeOfRoom,date,email,numOfRoom) {
   // email
@@ -11,17 +11,33 @@ module.exports = function confirmEmail(firstname,lastname,id,hotelName,typeOfRoo
     }
   });
 
-  var html = "<div></div>";
-  html += `<div>Dear ${firstname} ${lastname}</div>`;
-  html += `<div>Thank you for booking at Abode</div>`;
-  html += `<div>You booking ID is ${id}</div>`;
-  html += `<div>You reservation was made in ${hotelName} with ${numOfRoom} ${typeOfRoom} room(s)</div>`;
-  html += `<div>Please Check in before ${date.checkin}</div>`;
-  html += `<div>You can view your booking detail in www.abode.city/history-search </div>`
+  var html = `<body>
+  <div>
+  <div class="topBar" style="background-image:linear-gradient(to right, #0c4b78, #3d4e96, #2c76a9);;overflow: hidden; height:100px;"> 
+  <img src="cid:logo" alt="Smiley face" height="100" width="100" style="display: block;margin-left: auto;margin-right: auto;" />
+  </div>
+  <div style="margin: auto;  width: 50%;  width:500px">
+  <h1>Your Reservation is confirm</h1>
+  <h4>Dear ${firstname} ${lastname}</h4>
+  <p>Thank you for booking at Abode</p>
+  <p>You Reservation ID is ${id}</p>
+  <p>You reservation was made in ${hotelName} with ${numOfRoom} ${typeOfRoom} room(s)</p>
+  <p>We are expecting your arrival at ${date.checkin}</p>
+  <p>You can view your booking detail in <a href="www.abode.city/history-search">www.abode.city/history-search </a></p>
+  </div>
+  </div>
+  </body>`
   var mailOptions = {
     from: '"Abode Customer Service" <no-reply@abode.com>',
     to: `${email}`,
     subject: "[Order Confirmation]Your Revservation is confirmed",
+    attachments: [
+      {
+        filename: "logo.png",
+        path: __dirname + "/logo.png",
+        cid: "logo"
+      }
+    ],
     html: `${html}`
   };
   console.log("here");
