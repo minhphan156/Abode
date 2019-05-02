36b7115f6c1708cea78abaefe9c44605ad6ee445
yuta05 <yuta121305@gmail.com>
2019-05-02 13:15:50 -0700

    added image

--- front_end/src/components/disclaimer/Disclaimer.js
+++ front_end/src/components/disclaimer/Disclaimer.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
-import "./disclaimer.css";
+import "./Disclaimer.css";
 import SJSU from "../../images/SJSU.png";
 import Typography from "@material-ui/core/Typography";
 
