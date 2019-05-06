eedd3725da54b8d5fec81c6587592087ea9f085f
Sandro Sallenbach <hcl.sandro@hotmail.de>
2019-05-05 23:43:31 -0700

    last one

--- front_end/src/store.js
+++ front_end/src/store.js
@@ -9,7 +9,10 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(...middleware))
+  compose(
+    applyMiddleware(...middleware),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
